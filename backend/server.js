595030fbf774967da8893efa05d1bf8b3344b2e8
kalyan10001 <venkatakalyan1000@gmail.com>
2024-05-27 14:45:06 +0530

    post routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
@@ -24,6 +25,8 @@ app.use(express.urlencoded({extended:true}));
 app.use("/api/auth",authRoutes);
 
 app.use("/api/users",userRoutes);
+
+app.use("/api/posts",postRoutes);
 
 
 app.listen(PORT,()=>{
