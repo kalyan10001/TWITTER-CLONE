b9f6b9ccfa07bca188fc576962d337c64cb2b29f
kalyan10001 <venkatakalyan1000@gmail.com>
2024-05-28 16:39:38 +0530

    auth handled and messages protected in client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage.jsx";
 import SignUpPage from "./pages/auth/signup/SignUppage.jsx";
 import LoginPage from "./pages/auth/login/LoginPage.jsx";
@@ -6,19 +6,50 @@ import Sidebar from "./components/common/Sidebar.jsx";
 import RightPanel from "./components/common/RightPanel.jsx";
 import NotificationPage from "./pages/notification/NotificationPage.jsx";
 import ProfilePage from "./pages/profile/ProfilePage.jsx";
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner.jsx";
 
 function App() {
+	const { data:authUser,isError,error,isLoading } = useQuery({
+		queryKey: ["authUser"],
+		queryFn: async () => {
+			try {
+				const res = await fetch("/api/auth/me");
+				const data = await res.json();
+				if (data.error) return null;
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+				console.log("authUser is here:", data);
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		retry:false,
+	});
+
+	if (isLoading) {
+		return (
+			<div className='h-screen flex justify-center items-center'>
+				<LoadingSpinner size='lg' />
+			</div>
+		);
+	}
+ console.log(authUser);
 	return (
 		<div className='flex max-w-6xl mx-auto'>
-      <Sidebar/>
+      {authUser &&<Sidebar/>}
 			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/signup' element={<SignUpPage />} />
-				<Route path='/login' element={<LoginPage />} />
-				<Route path='/notifications' element={<NotificationPage />} />
-				<Route path='/profile/:username' element={<ProfilePage />} />
+				<Route path='/' element={authUser?<HomePage />:<Navigate to="/login"/>} />
+				<Route path='/signup' element={!authUser?<SignUpPage />:<Navigate to="/"/>} />
+				<Route path='/login' element={!authUser?<LoginPage />:<Navigate to="/"/>} />
+				<Route path='/notifications' element={authUser?<NotificationPage />:<Navigate to="/"/>} />
+				<Route path='/profile/:username' element={authUser?<ProfilePage />:<Navigate to="/login"/>} />
 			</Routes>
-      <RightPanel/>
+      {authUser && <RightPanel/>}
+	  <Toaster/>
 		</div>
 	);
 };
--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -5,6 +5,8 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const Sidebar = () => {
 	const data = {
@@ -12,6 +14,34 @@ const Sidebar = () => {
 		username: "johndoe",
 		profileImg: "/avatars/boy1.png",
 	};
+const queryClient=useQueryClient();
+
+	const {mutate:logout,isPending,isError,error}=useMutation({
+		mutationFn:async()=>{
+			try {
+				const res=await fetch("/api/auth/logout",{
+					method:"POST",
+				})
+				const data= await res.json();
+				console.log(data)
+				if(!res.ok){
+					throw new Error(data.error || "something went wrong");
+				}
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess:()=>{
+			toast.success("logout successfully");
+			queryClient.invalidateQueries({querykey:["authUser"]});
+		},
+		onError:()=>{
+			toast.error("logout successful")
+		}
+
+	});
+
+	const {data:authUser}=useQuery({queryKey:["authUser"]});
 
 	return (
 		<div className='md:flex-[2_2_0] w-18 max-w-52'>
@@ -51,20 +81,25 @@ const Sidebar = () => {
 				</ul>
 				{data && (
 					<Link
-						to={`/profile/${data.username}`}
+						to={`/profile/${authUser.username}`}
 						className='mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full'
 					>
 						<div className='avatar hidden md:inline-flex'>
 							<div className='w-8 rounded-full'>
-								<img src={data?.profileImg || "/avatar-placeholder.png"} />
+								<img src={authUser?.profileImg || "/avatar-placeholder.png"} />
 							</div>
 						</div>
 						<div className='flex justify-between flex-1'>
 							<div className='hidden md:block'>
-								<p className='text-white font-bold text-sm w-20 truncate'>{data?.fullName}</p>
-								<p className='text-slate-500 text-sm'>@{data?.username}</p>
+								<p className='text-white font-bold text-sm w-20 truncate'>{authUser?.fullName}</p>
+								<p className='text-slate-500 text-sm'>@{authUser?.username}</p>
 							</div>
-							<BiLogOut className='w-5 h-5 cursor-pointer' />
+							<BiLogOut className='w-5 h-5 cursor-pointer' 
+							onClick={(e)=>{
+								e.preventDefault();
+								logout();
+							}}
+							/>
 						</div>
 					</Link>
 				)}
