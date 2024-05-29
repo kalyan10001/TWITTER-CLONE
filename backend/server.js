ee0cdeb2c0cd87a862d042aed5f65d163ccd4ef6
kalyan10001 <venkatakalyan1000@gmail.com>
2024-05-29 16:13:26 +0530

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
@@ -17,8 +18,9 @@ cloudinary.config(
     }
 );
 const app=express();
-const PORT=process.env.PORT || 8000
+const PORT=process.env.PORT || 5000
 
+const __dirname=path.resolve();
 app.use(express.json({limit:"5mb"}));
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
@@ -30,6 +32,14 @@ app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
 
 app.use("/api/notifications",notificationRoutes);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 
 app.listen(PORT,()=>{
